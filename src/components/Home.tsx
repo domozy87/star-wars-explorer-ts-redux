ecb8d56557ac041d454afbc913c79b2aa43bcc31
DomArcurve <89416279+DomArcurve@users.noreply.github.com>
2021-12-04 23:08:53 -0400

    Add default AppBar and buttons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// MUI
+import { Button } from '@mui/material';
 
 // Conponents
 import Header from './Header'
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: 20px;
+`;
+
+const StyleButton = styled(Button)`
+    min-width: 150px;
+    margin: 10px 0;
+`;
 
 const Home: React.FC = () => {
     return (
         <>
             <Header title="Star Wars Explorer" />
+            <ButtonContainer>
+                <StyleButton variant="contained" href="/people" size="large">People</StyleButton>
+                <StyleButton variant="contained" href="/movies" size="large">Movies</StyleButton>
+                <StyleButton variant="contained" href="/planets" size="large">Planets</StyleButton>
+            </ButtonContainer>
         </>
     );
 };
